9fad6e47377c5b95fe98ab23d407ac3fa5f949d5
fumihiko kakuma <kakuma@valinux.co.jp>
2014-11-06 20:15:24 +0900

    add replacing a localrc file.

--- devstack-vm-gate.sh
+++ devstack-vm-gate.sh
@@ -378,6 +378,10 @@ EOF
 else
     cd $BASE/new/devstack
     setup_localrc "new" "$OVERRIDE_ZUUL_BRANCH"
+    if [ -n "$DEVSTACK_GATE_3PLOCALRC" -a -e "$DEVSTACK_GATE_3PLOCALRC" ]; then
+        echo "Replace a localrc with $DEVSTACK_GATE_3PLOCALRC"
+        cp $DEVSTACK_GATE_3PLOCALRC localrc
+    fi
 
     # Make the workspace owned by the stack user
     sudo chown -R stack:stack $BASE
